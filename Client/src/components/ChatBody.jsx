6eafa22e1c64b45a13d5fe485c1e5ce291f1280d
Chalana Shehara <73055643+Chalana-98@users.noreply.github.com>
2023-10-23 18:23:53 +0530

    backend setup
    backend setup

--- Client/src/components/ChatBody.jsx
+++ Client/src/components/ChatBody.jsx
@@ -1,24 +1,43 @@
-import React from "react";
+import React, { useRef } from "react";
+import autoAnimate from "@formkit/auto-animate";
+import { useEffect } from "react";
 
-const ChatBody = () => {
-    const aiStyle ="bg-white bg-opacity-40 backdrop-blur-lg dropshadow-md mr-auto";
+
+const ChatBody = ({ chat }) => {
+  const aiStyle =
+    "bg-white bg-opacity-40 backdrop-blur-lg dropshadow-md mr-auto";
+
+    const parent = useRef(null);
+    const bottomRef = useRef(null);
+
+    // only for aut animations
+    useEffect(()=>{
+        parent.current && autoAnimate(parent.current);
+    }, [parent])
+
+    //for scrolling bottom
+    useEffect(()=>{
+        bottomRef.current?.scrollIntoView({behavior: "smooth"})
+    }, [chat])
+
   return (
-    <div className=" flex flex-col gap-4">
-      {/* client message */}
-      <div className=" border-[#999999] break-words border-2 rounded-xl self-end px-3 py-3 max-w-[80%]">
-        <pre className=" whitespace-pre-wrap">
-          <span>Hy ChatMuse , Can you help me?</span>
-        </pre>
-      </div>
+    <div className="flex flex-col gap-4" ref={parent}>
+      {chat.map((message, i) => {
+        return (
+          <div
+            key={i}
+            className={`border-[#999999] break-words border-2 rounded-xl self-end px-3 py-3 max-w-[80%] ${
+              message.sender === "ai" && aiStyle
+            }`}
+          >
+            <pre className="whitespace-pre-wrap">
+              <span>{message.message}</span>
+            </pre>
+          </div>
+        );
+      })}
 
-{/* ai message */}
-      <div className="border-[#999999] break-words border-2 rounded-xl self-end px-3 py-3 max-w-[80%]">
-        
-        
-        <pre className=" whitespace-pre-wrap ">
-          <span>Yes , I Can help you </span>
-        </pre>
-      </div>
+      <div ref={bottomRef} className="h-3"></div>
     </div>
   );
 };
